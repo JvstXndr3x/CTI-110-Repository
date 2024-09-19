98bde2154ea60a21fd6458a6c69322dd98b58cbb
JvstXndr3x <mejiasa3102@student.faytechcc.edu>
2024-09-19 10:38:53 -0400

    Add files via upload
    First submission on Module 3 Python 1.

---
+++ P1HW1_MejiasAndrea.py
@@ -0,0 +1,13 @@
+# Andrea Mejias
+# 9/17/2024
+# P1HW1
+# Input and output with mathematical expressions
+
+print("----Calculating Exponents----")
+print()
+print()
+# Get base value (as an integer) from the user
+baseValue = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+
+
